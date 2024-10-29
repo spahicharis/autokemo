6fd0428733b260bfd7b9cef55c4914c173244bac
Haris Spahić <spahaa@hotmail.com>
2024-10-29 16:03:51 +0100

    Wip

--- apps/autokemo-public/src/app/layout/app.navbar.component.ts
+++ apps/autokemo-public/src/app/layout/app.navbar.component.ts
@@ -8,18 +8,22 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [CommonModule, RouterLink, RouterLinkActive],
   template: `
     <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow sticky-top p-0">
-      <a routerLink="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-        <h2 class="m-0 text-light"><i class="fa fa-car me-3"></i><img src="autokemo-white-yellow-2.png" alt=""></h2>
-      </a>
+      <div class="d-flex flex-column">
+        <a routerLink="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+          <h2 class="m-0 text-light"><i class="fa fa-car me-3"></i><img src="autokemo-white-yellow-2.png" alt=""></h2>
+        </a>
+        <span class="px-4 px-lg-5 text-xs sm:text-sm text-gray-400">Uvoz i prodaja | Otpad | Servis | Autolakirer | Auto stakla</span>
+      </div>
+
       <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0 gap-3 me-2">
-          <a class="nav-item nav-link">O nama</a>
-          <a href="service.html" class="nav-item nav-link">Usluge</a>
+          <a routerLink="" class="nav-item nav-link active">O nama</a>
+          <a routerLink="" fragment="usluge" class="nav-item nav-link active">Usluge</a>
           <div class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+            <a href="#" class="nav-link active dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
             <div class="dropdown-menu fade-up m-0">
               <a href="booking.html" class="dropdown-item">Booking</a>
               <a href="team.html" class="dropdown-item">Technicians</a>
@@ -27,7 +31,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
               <a href="404.html" class="dropdown-item">404 Page</a>
             </div>
           </div>
-          <a href="contact.html" class="nav-item nav-link">Kontakt</a>
+          <a href="contact.html" class="nav-item nav-link active">Kontakt</a>
         </div>
         <a href="https://olx.ba/shops/autootpadkemo/aktivni"
            target="_blank"
